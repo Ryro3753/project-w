c900ed276e9fa29436cbcadc0f875f9ec9554b4a
ibrahim.ozdemir <ibrahim.ozdemir@outlook.com.tr>
2022-01-09 23:30:23 +0300

    api init

---
+++ API/API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+            return base.SaveChanges();
+        }
+
+    }
+}
