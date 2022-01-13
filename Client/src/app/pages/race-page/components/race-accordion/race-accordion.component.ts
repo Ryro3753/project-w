614a28fa93cc681c0ffc8ee4d41ddd3878ca5bf6
ibrahim.ozdemir <ibrahim.ozdemir@outlook.com.tr>
2022-01-13 14:26:36 +0300

    race accordion init

--- Client/src/app/pages/race-page/components/race-accordion/race-accordion.component.ts
+++ Client/src/app/pages/race-page/components/race-accordion/race-accordion.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Race } from 'src/app/models/races.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-race-accordion',
@@ -12,7 +13,18 @@ export class RaceAccordionComponent implements OnInit {
 
   @Input() race: Race | undefined;
 
+  details: boolean = false;
+
+  apiURL = environment.apiURL;
+  noImagePath = this.apiURL + '/images/miscimages/no-image.svg';
+  raceImageBasePath = this.apiURL + '/images/raceImages/';
+
+
   ngOnInit(): void {
+  }
+
+  detailsClick(){
+    this.details = !this.details;
   }
 
 }
